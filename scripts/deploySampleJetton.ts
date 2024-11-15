a26393377d44e7aa6d7444517b379cab344c0e66
Vadim <vad99lord@gmail.com>
2024-11-16 01:46:14 +0300

    add test deploy token

--- scripts/deploySampleJetton.ts
+++ scripts/deploySampleJetton.ts
@@ -1,34 +1,44 @@
-import { Address, toNano } from '@ton/core';
-import { SampleJetton } from '../wrappers/SampleJetton';
 import { NetworkProvider } from '@ton/blueprint';
+import { Address, toNano } from '@ton/core';
 import { buildOnchainMetadata } from '../utils/jetton-helpers';
+import { SampleJetton } from '../wrappers/SampleJetton';
 
 export async function run(provider: NetworkProvider) {
+    console.log(provider.network());
     const jettonParams = {
-        name: "Nikandr Token",
-        description: "Official token of the Nikandr Surkov Youtube channel :) https://www.youtube.com/@NikandrSurkov",
-        symbol: "NIKANDR",
-        image: "https://violet-traditional-rabbit-103.mypinata.cloud/ipfs/QmUgZ3kWg36tCVSZeVKXkvsdXkn6dqigqjoBZto9Y8h37z",
+        name: 'SCUM Token',
+        description: "You've been scummed, loser",
+        symbol: 'SCUM',
+        image: '',
     };
 
     // Create content Cell
     let content = buildOnchainMetadata(jettonParams);
 
-    const sampleJetton = provider.open(await SampleJetton.fromInit(provider.sender().address as Address, content, 1000000000000000000n));
+    // Deploy the SampleJetton contract
+    const sampleJetton = provider.open(
+        await SampleJetton.fromInit(
+            provider.sender().address as Address,
+            content,
+            1000000000000000000n, // Set max_supply
+        ),
+    );
 
+    // Send the initial transaction to deploy the contract
     await sampleJetton.send(
         provider.sender(),
         {
-            value: toNano('0.05'),
+            value: toNano('0.05'), // Deployment value
         },
         {
             $$type: 'Mint',
-            amount: 100000000000000000n,
-            receiver: provider.sender().address as Address
-        }
+            amount: 100000000000000000n, // Initial mint amount
+            receiver: provider.sender().address as Address, // Receiver address
+        },
     );
 
+    // Wait for the contract to deploy
     await provider.waitForDeploy(sampleJetton.address);
 
-    // run methods on `sampleJetton`
+    console.log(`SampleJetton deployed at address: ${sampleJetton.address}`);
 }
